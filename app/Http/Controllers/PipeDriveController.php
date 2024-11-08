17261b03aeebfd7f4ace9a65211d7d37e8a4db2a
HugoFalci <hugo.flc.ribeiro@gmail.com>
2024-11-08 16:20:36 -0300

    Adicionando o SyncController

--- app/Http/Controllers/PipeDriveController.php
+++ app/Http/Controllers/PipeDriveController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class PipeDriveController extends Controller
 {
@@ -14,5 +15,64 @@ class PipeDriveController extends Controller
     {
         $this->apiToken = env('PIPEDRIVE_API_TOKEN');
         $this->baseUrl = env('PIPEDRIVE_BASE_URL');
+    }
+
+    /**
+     * Retrieve all deals from Pipedrive.
+     */
+    public function getDeals()
+    {
+        $url = "{$this->baseUrl}/v1/deals";
+        $params = ['api_token' => $this->apiToken];
+
+        $response = Http::get($url, $params);
+
+        if ($response->successful()) {
+            return response()->json($response->json());
+        } else {
+            Log::error("Failed to retrieve deals from Pipedrive", ['response' => $response->body()]);
+            return response()->json(['error' => 'Request failed'], $response->status());
+        }
+    }
+
+    /**
+     * Retrieve a specific deal by ID from Pipedrive.
+     * 
+     * @param int $id Deal ID
+     */
+    public function getDeal($id)
+    {
+        $url = "{$this->baseUrl}/v1/deals/{$id}";
+        $params = ['api_token' => $this->apiToken];
+
+        $response = Http::get($url, $params);
+
+        if ($response->successful()) {
+            return response()->json($response->json());
+        } else {
+            Log::error("Failed to retrieve deal with ID {$id} from Pipedrive", ['response' => $response->body()]);
+            return response()->json(['error' => 'Request failed'], $response->status());
+        }
+    }
+    
+    /**
+     * Update an existing deal in Pipedrive.
+     * 
+     * @param Request $request
+     * @param int $id Deal ID
+     */
+    public function updateDeal(Request $request, $id)
+    {
+        $url = "{$this->baseUrl}/v1/deals/{$id}";
+        $data = array_merge($request->all(), ['api_token' => $this->apiToken]);
+
+        $response = Http::put($url, $data);
+
+        if ($response->successful()) {
+            return response()->json($response->json());
+        } else {
+            Log::error("Failed to update deal with ID {$id} in Pipedrive", ['data' => $data, 'response' => $response->body()]);
+            return response()->json(['error' => 'Request failed'], $response->status());
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,30 @@
 <?php
 
 use App\Http\Controllers\EvoTalksController;
+use App\Http\Controllers\PipeDriveController;
+use App\Http\Controllers\SyncController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/create-opportunity', [EvoTalksController::class, 'createOpportunity']);
-Route::post('/update-opportunity', [EvoTalksController::class, 'updateOpportunity']);
-Route::post('/remove-opportunity', [EvoTalksController::class, 'removeOpportunity']);
-Route::post('/lose-opportunity', [EvoTalksController::class, 'loseOpportunity']);
-Route::post('/win-opportunity', [EvoTalksController::class, 'winOpportunity']);
-Route::post('/transfer-opportunity', [EvoTalksController::class, 'transferOpportunity']);
-Route::post('/change-opportunity-stage', [EvoTalksController::class, 'changeOpportunityStage']);
-Route::post('/insert-opportunity-note', [EvoTalksController::class, 'insertOpportunityNote']);
-Route::post('/getOpportunity', [EvoTalksController::class, 'getOpportunity']);
-Route::post('/get-pipe-opportunities', [EvoTalksController::class, 'getPipeOpportunities']);
+Route::prefix('evotalks')->group(function () {
+    Route::post('/create-opportunity', [EvoTalksController::class, 'createOpportunity']);
+    Route::post('/update-opportunity', [EvoTalksController::class, 'updateOpportunity']);
+    Route::post('/remove-opportunity', [EvoTalksController::class, 'removeOpportunity']);
+    Route::post('/lose-opportunity', [EvoTalksController::class, 'loseOpportunity']);
+    Route::post('/win-opportunity', [EvoTalksController::class, 'winOpportunity']);
+    Route::post('/transfer-opportunity', [EvoTalksController::class, 'transferOpportunity']);
+    Route::post('/change-opportunity-stage', [EvoTalksController::class, 'changeOpportunityStage']);
+    Route::post('/insert-opportunity-note', [EvoTalksController::class, 'insertOpportunityNote']);
+    Route::post('/getOpportunity', [EvoTalksController::class, 'getOpportunity']);
+    Route::post('/get-pipe-opportunities', [EvoTalksController::class, 'getPipeOpportunities']);
+});
+
+Route::prefix('pipedrive')->group(function () {
+    Route::get('/deals', [PipeDriveController::class, 'getDeals']);
+    Route::get('/deal/{id}', [PipeDriveController::class, 'getDeal']);
+    Route::put('/deal/{id}', [PipeDriveController::class, 'updateDeal']);
+});
+
+Route::prefix('sync')->group(function () {
+    Route::post('/pipedrive/create-opportunity', [SyncController::class, 'syncCreatedOpportunityFromPipeDrive']);
+    Route::post('/pipedrive/update-opportunity', [SyncController::class, 'syncUpdatedOpportunityFromPipeDrive']);
+});
